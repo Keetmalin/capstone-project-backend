3ee42ad53efad7608080d342c994f64946a0a675
jxhzxn <jahzanahamed99@gmail.com>
2022-05-25 16:32:34 +0530

    added search functionality

--- frontend/src/app/components/student-table/student-table.component.ts
+++ frontend/src/app/components/student-table/student-table.component.ts
@@ -2,14 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Router,NavigationExtras } from '@angular/router';
 import { faTrash, faPlus, faPenSquare } from '@fortawesome/free-solid-svg-icons';
 import {AppServiceService} from '../../app-service.service';
-
 @Component({
   selector: 'app-student-table',
   templateUrl: './student-table.component.html',
   styleUrls: ['./student-table.component.css']
 })
 export class StudentTableComponent implements OnInit {
-
 
   faTrash = faTrash;
   faPlus = faPlus;
@@ -38,7 +36,7 @@ export class StudentTableComponent implements OnInit {
 
   getStudentData(){
     this.service.getStudentData().subscribe((response)=>{
-      this.studentData = response;
+      this.studentData = Object.keys(response).map((key) => [response[key]]);
     },(error)=>{
       console.log('ERROR - ', error)
     })
@@ -51,5 +49,19 @@ export class StudentTableComponent implements OnInit {
     this.service.deleteStudent(student).subscribe((response)=>{
       this.getStudentData()
     })
+  }
+
+  search(value) {
+    let foundItems = [];
+    if (value.length <= 0) {
+      this.getStudentData();
+    } else {
+      let b = this.studentData.filter((student) => {
+        if (student[0].name.toLowerCase().indexOf(value) > -1) {
+          foundItems.push(student)
+        }
+      });
+      this.studentData = foundItems;
+    }
   }
 }
